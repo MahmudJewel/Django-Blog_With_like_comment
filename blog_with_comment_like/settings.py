90b5384ac7f59e061a134857cd5799ca6a3f8350
jewel <jmbengal.mbstu@gmail.com>
2022-04-11 04:04:31 +0600

    login, logout added

--- blog_with_comment_like/settings.py
+++ blog_with_comment_like/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     
     # external app 
     'post',
+    'home',
+    
+    # 3rd party app 
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +61,7 @@ ROOT_URLCONF = 'blog_with_comment_like.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# after login view 
+LOGIN_REDIRECT_URL='/afterlogin'
--- post/models.py
+++ post/models.py
@@ -1,22 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
+# Main category for blog post 
 class Category(models.Model):
     title = models.CharField(max_length=200)
     
     def __str__(self):
         return self.title
 
+# Blog post model 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    slug = models.SlugField(max_length=200)
     title = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=200, blank=True, null=True)
     post = models.TextField()
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
     
     def __str__(self):
         return f"{self.author} ==> {self.title}"
+    
+    # override save for slugify 
+    def save(self, *args, **kwargs):
+        original_slug = slugify(self.title)
+        queryset = Post.objects.all().filter(slug__iexact=original_slug).count()
+
+        # if title matches, then modify slug fields 
+        count = 1
+        slug = original_slug
+        while(queryset):
+            slug = original_slug + '-' + str(count)
+            count += 1
+            queryset = Post.objects.all().filter(slug__iexact=slug).count()
+
+        self.slug = slug
+        # end modifying slug field 
+        super(Post, self).save(*args, **kwargs)
